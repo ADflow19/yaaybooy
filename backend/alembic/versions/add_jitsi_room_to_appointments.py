e90450cb215b6c757b1369cd820b452b4d24e56c
ADflow19 <mbayeablaye8@gmail.com>
2026-07-12 17:42:51 +0000

    Set down_revision for Jitsi room migration

--- backend/alembic/versions/add_jitsi_room_to_appointments.py
+++ backend/alembic/versions/add_jitsi_room_to_appointments.py
@@ -10,7 +10,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision: str = "a1b2c3d4e5f6"
-down_revision: Union[str, None] = None
+down_revision: Union[str, None] = "6b4c1a2ba2d1"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
